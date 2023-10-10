45ae4b627b5657bfbab152a7df29c0cd352f3c77
wyy <982429087@qq.com>
2023-10-10 09:36:48 +0800

    feat: 更新

--- docs/examples/button/basic.vue
+++ docs/examples/button/basic.vue
@@ -1,14 +1,15 @@
 <template>
-  <FaButton :type="'primary'" :wait="100" @click="handleClick"
+  <FaButton :type="'primary'" :wait="400" @click="handleClick"
     >连续点我开始</FaButton
   >
 </template>
 
 <script setup lang="ts">
+import { ElMessage } from 'element-plus'
 import { FaButton } from '@farst/components'
 
 function handleClick() {
-  console.log('click button')
+  ElMessage.info('点击了按钮')
 }
 </script>
 
--- packages/components/button/src/button.vue
+++ packages/components/button/src/button.vue
@@ -1,10 +1,17 @@
 <template>
   <el-button v-bind="elProps" v-on="elEmits" @click="handleClick">
     <slot />
+    <template v-if="slots.loading" #loading>
+      <slot name="loading" />
+    </template>
+    <template v-if="slots.icon" #icon>
+      <slot name="icon" />
+    </template>
   </el-button>
 </template>
 
 <script lang="ts" setup>
+import { useSlots } from '@vue/runtime-core'
 import {
   ElButton,
   buttonEmits as elButtonEmits,
@@ -20,6 +27,8 @@ defineOptions({
 
 const props = defineProps(buttonProps)
 const emit = defineEmits(buttonEmits)
+const slots = useSlots()
+
 const elProps = useBindProps(props, elButtonProps)
 const elEmits = useOnEmits(emit, elButtonEmits, ['click'])
 
